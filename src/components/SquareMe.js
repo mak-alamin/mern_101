fb60073e131dd4771f2974ad286f2e39237bbb9f
Mak Alamin <makalamin012@gmail.com>
2021-10-27 03:21:06 +0600

    Learning webpack

---
+++ src/components/SquareMe.js
@@ -0,0 +1,5 @@
+function SquareMe(x) {
+    return x * x;
+}
+
+export default SquareMe;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import validator from "validator";
+import SquareMe from "./components/SquareMe";
+
+console.log(validator.isEmail("john@yahoo.com"));
+
+console.log(SquareMe(10));
