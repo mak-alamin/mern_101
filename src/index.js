c25ca0760c275c7ca36dbeb5afb6250b86984386
Mak Alamin <makalamin012@gmail.com>
2021-11-02 09:20:56 +0600

    React setup successfully

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
 
 import SquareMe from "./components/SquareMe"
 import Rectangle from "./components/Rectangle"
 
 import "./index.html"
+
 
 let rect1 = new Rectangle(4, 20)
 console.log(rect1.area)
@@ -10,3 +13,10 @@ console.log(rect1.area)
 console.log(rect1.a);
 
 console.log(SquareMe(10));
+
+
+function App() {
+    return <h1>Hello From MERN World!</h1>
+}
+
+ReactDOM.render(<App />, document.getElementById("app"))
--- webpack.config.js
+++ webpack.config.js
@@ -17,11 +17,11 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
+                exclude: /(node_modules)/,
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-env']
+                        presets: ['@babel/preset-env', '@babel/preset-react']
                     }
                 }
             },
