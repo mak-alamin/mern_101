7087f147b7b9aa1891995ce101dd0ba49835817c
Mak Alamin <makalamin012@gmail.com>
2021-11-03 00:13:05 +0600

    Cache busting

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,13 @@ const currentTask = process.env.npm_lifecycle_event
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 const config = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.[hash].js'
     },
     mode: 'development',
     devServer: {
@@ -51,7 +52,7 @@ const config = {
 if ("build" == currentTask) {
     config.mode = "production"
     config.module.rules[0].use[0] = MiniCssExtractPlugin.loader
-    config.plugins.push(new MiniCssExtractPlugin({ filename: 'style.css' }))
+    config.plugins.push(new MiniCssExtractPlugin({ filename: 'style.[hash].css' }), new CleanWebpackPlugin())
 }
 
 module.exports = config
