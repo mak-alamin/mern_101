b7bffd187f03e5cc0e95990a9544db7e98fc1665
Mak Alamin <makalamin012@gmail.com>
2022-01-29 00:10:54 +0600

    created handle form function

--- src/components/AddTaskForm.js
+++ src/components/AddTaskForm.js
@@ -1,11 +1,15 @@
 import React from "react";
 
+function handleForm(){
+    console.log("Form submitted");
+}
+
 // Add new Task form
 function AddTaskForm() {
     return (
         <div>
-            <form>
-                <input type="text" name="task" placeholder="Add new task" /> <br />
+            <form onSubmit={handleForm()}>
+                <input type="text" name="task" id="my_task" placeholder="Add new task" /> <br />
                 <input type="submit" value="Add Task" />
             </form>
         </div>
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -4,7 +4,7 @@ function ToDoList() {
     return (
         <div>
             <h3>Need to do today</h3>
-            <ul>
+            <ul id="to_do_list">
                 <li>Learn React</li>
                 <li>Learn Redux</li>
                 <li>Learn React Router</li>
